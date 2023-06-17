6a7baebe3b9383a61f4040b0797b3bc4e9269419
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2023-06-17 19:52:02 +0000

    Deploying to gh-pages from @ nastyaosetrova8/goit-js-hw-09@5eac3c20c264975a3f5c8cc6ab03f320bc8031bf 🚀

---
+++ 01-color-switcher.ed67a7e0.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]"),n=document.querySelector("body");e.addEventListener("click",(function(){t=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),e.disabled=!0,a.disabled=!1})),a.addEventListener("click",(function(){a.disabled=!0,e.disabled=!1,clearInterval(t),t=null}))}();
+//# sourceMappingURL=01-color-switcher.ed67a7e0.js.map
